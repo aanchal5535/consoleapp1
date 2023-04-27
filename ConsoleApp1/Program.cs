cadd8dced88e57372c68847ffffb5aed0eef9d08
aanchal-subedi <aanchalsubedi005@gmail.com>
2023-04-27 12:58:16 +0545

    bhf

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using  System;
+class printdata
+    {
+        void print(int i)
+        {
+        Console.WriteLine("printing int:{0}", i);
+        }
+    void print(double f)
+    {
+        Console.WriteLine("printing int:{0}", f);
+
+    }
+    void print(string s)
+    {
+        Console.WriteLine("printing int:{0}", s);
+    }
+    static void Main(string[] args)
+    {
+        printdata printdata = new printdata();
+        printdata.print(5);
+        printdata.print(500.263);
+        printdata.print("Hello C++");
+            Console.ReadKey();
+    }
+    }
